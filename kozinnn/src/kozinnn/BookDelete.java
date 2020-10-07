59f9cf73dd59a0fd25293a6c501f37a04c8b9a09
rwd117 <rwd117@naver.com>
2020-10-07 21:39:12 +0900

    검색 기능 넣기

--- kozinnn/src/kozinnn/BookDelete.java
+++ kozinnn/src/kozinnn/BookDelete.java
@@ -28,7 +28,8 @@ public class BookDelete  extends JInternalFrame implements ActionListener{
 	private JTextField tf4;
 	private JTable table;
 	private JButton Btok, Btca, btn1, btn2, btn3, btn4,btnexit;
-
+	private JLabel lblll ;
+	
 	private final static int A = 0;
 	private final static int B = 1;
 	private final static int C = 2;
@@ -116,7 +117,7 @@ public class BookDelete  extends JInternalFrame implements ActionListener{
 
 	private void initialize() {
 		this.getContentPane().setLayout(null);
-		this.setTitle("책 수정");
+		this.setTitle("책 삭제");
 
 		JLabel lblNewLabel = new JLabel("\uCC45 \uCF54\uB4DC");
 		lblNewLabel.setBounds(24, 115, 88, 20);
@@ -203,6 +204,10 @@ public class BookDelete  extends JInternalFrame implements ActionListener{
 		btnexit.setBounds(24, 10, 30, 23);
 		this.getContentPane().add(btnexit);
 		
+		lblll = new JLabel("\uD65C\uC131\uD654 \uBC84\uD2BC\uC744 \uB20C\uB7EC\uC8FC\uC138\uC694 (\uBCF5\uC218 \uC120\uD0DD \uBD88\uAC00)");
+		lblll.setBounds(12, 43, 339, 47);
+		this.getContentPane().add(lblll);
+		
 
 		Btok.addActionListener(this);
 		Btca.addActionListener(this);
@@ -217,7 +222,243 @@ public class BookDelete  extends JInternalFrame implements ActionListener{
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
+		if (e.getSource().equals(Btok)) {
+			if (cmd == A) {
+				bpla();
+				to();
+			} else if (cmd == B) {
+				bplb();
+				to();
+			} else if (cmd == C) {
+				bplc();
+				to();
+			} else if (cmd == D) {
+				bpld();
+				to();
+			} else if (cmd == E) {
+				bple();
+				to();
+			} else if (cmd == F) {
+				bplf();
+				to();
+			}
+		} else if (e.getSource().equals(Btca)) {
+			set();
+		} else if (e.getSource().equals(btn1)) {
+			if(cmd==B) {
+				tf.setEnabled(false);
+				cmd=A;
+				set();
+				return;
+			}
+			tb1();
+			cmd = B;
+			
+		} else if (e.getSource().equals(btn2)) {
+			if(cmd==C) {
+				tf1.setEnabled(false);
+				cmd=A;
+				set();
+				return;
+			}
+			tb2();
+			cmd = C;
+		} else if (e.getSource().equals(btn3)) {
+			if(cmd==D) {
+				tf2.setEnabled(false);
+				cmd=A;
+				set();
+				return;
+			}
+			tb3();
+			cmd = D;
+		} else if (e.getSource().equals(btn4)) {
+			if(cmd==E) {
+				tf3.setEnabled(false);
+				cmd=A;
+				set();
+				return;
+			}
+			tb4();
+			cmd = E;
+		} else if(e.getSource().equals(btn5)) {
+			if(cmd==F) {
+				tf4.setEnabled(false);
+				cmd=A;
+				set();
+				return;
+			}
+			tb5();
+			cmd=F;
+			
+		}else if(e.getSource().equals(btnexit)) {
+			subCloseWindow();
+		}
+	}
+	
+	public void bpla() {
+		lblll.setText("활성화를 누른 후 삭제를 눌러주세요. 복수 선택 불가능");
+	}
+	
+	
+	public void bplb() {
+		// b_code,b_number,b_title,b_name,b_ju,b_in
+		String code=tf.getText();
+		
+		try {
+
+			pst = con.prepareStatement(sqlDeletec);
+			pst.setString(1, code);
+			int res = pst.executeUpdate();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				pst.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public void bplc() {
+		// b_code,b_number,b_title,b_name,b_ju,b_in
+		String number=tf1.getText();
+		
+		try {
+
+			pst = con.prepareStatement(sqlDeleten);
+			pst.setString(1, number);
+			int res = pst.executeUpdate();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				pst.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	public void bpld() {
+		// b_code,b_number,b_title,b_name,b_ju,b_in
+		String title=tf2.getText();
+		try {
+
+			pst = con.prepareStatement(sqlDeletet);
+			pst.setString(1, title);
+			int res = pst.executeUpdate();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				pst.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	public void bple() {
+		// b_code,b_number,b_title,b_name,b_ju,b_in
+		String name=tf3.getText();
+		
+		try {
+
+			pst = con.prepareStatement(sqlDeletena);
+			pst.setString(1, name);
+			int res = pst.executeUpdate();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				pst.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	public void bplf() {
+		// b_code,b_number,b_title,b_name,b_ju,b_in
+		String ju=tf4.getText();
+		
+		try {
+
+			pst = con.prepareStatement(sqlDeletej);
+			pst.setString(1, ju);
+			int res = pst.executeUpdate();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				pst.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+
+	
+	public void tb1() {
+		tf.setEnabled(true);
+		tf1.setEnabled(false);
+		tf2.setEnabled(false);
+		tf3.setEnabled(false);
+		tf4.setEnabled(false);
 		
 	}
 
+	public void tb2() {
+		tf.setEnabled(false);
+		tf1.setEnabled(true);
+		tf2.setEnabled(false);
+		tf3.setEnabled(false);
+		tf4.setEnabled(false);
+	}
+
+	public void tb3() {
+		tf.setEnabled(false);
+		tf1.setEnabled(false);
+		tf2.setEnabled(true);
+		tf3.setEnabled(false);
+		tf4.setEnabled(false);
+	}
+
+	public void tb4() {
+		tf.setEnabled(false);
+		tf1.setEnabled(false);
+		tf2.setEnabled(false);
+		tf3.setEnabled(true);
+		tf4.setEnabled(false);
+	}
+
+	public void tb5() {
+		tf.setEnabled(false);
+		tf1.setEnabled(false);
+		tf2.setEnabled(false);
+		tf3.setEnabled(false);
+		tf4.setEnabled(true);
+	}
+	
+	public void subCloseWindow() {
+	       
+		try {
+           
+        	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        setVisible(false);
+        dispose();
+    }
 }
