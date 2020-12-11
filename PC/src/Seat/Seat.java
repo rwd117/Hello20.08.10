6db20731f2ae49ac079368976294073fbd3e09ad
rwd117 <rwd117@naver.com>
2020-12-11 15:34:41 +0900

    pc방 만들어보기

---
+++ PC/src/Seat/Seat.java
@@ -0,0 +1,52 @@
+package Seat;
+
+import java.awt.EventQueue;
+
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import Graphics.RoundButton;
+
+public class Seat extends JPanel {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Seat window = new Seat();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Seat() {
+		initialize();
+	}
+	
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 1263, 740);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JButton btnNewButton = new JButton("New button");
+		btnNewButton.setBounds(126, 104, 105, 27);
+		frame.getContentPane().add(btnNewButton);
+	}
+	
+}
