51018bbe9566abddb2e728e6d5ac009a6fda8713
rwd117 <rwd117@naver.com>
2020-12-17 20:16:54 +0900

    일단 콤보박스만

--- PC/src/User/PCuserLogin.java
+++ PC/src/User/PCuserLogin.java
@@ -15,6 +15,7 @@ import javax.swing.JTextField;
 
 import Db.UserLoginDb;
 import Main.MainPc;
+import Set.AllNum;
 
 public class PCuserLogin extends JFrame implements ActionListener {
 
@@ -22,11 +23,12 @@ public class PCuserLogin extends JFrame implements ActionListener {
 	private JTextField IDText;
 	private JPasswordField PWDText;
 	private JButton BtnLogin, BtnClear;
-	private JLabel IDlb, PWDlb;
-	private String Name = "아무개";
+	private JLabel IDlb, PWDlb, pcseat;
+	private JComboBox Jcom;
+	private String Seat[];
+	private String Pccombo;
 //	private ClientBackGround client=new ClientBackGround();
 //	private PCServerBackGround server=new PCServerBackGround();
-	private String msg;
 
 	public PCuserLogin() {
 		initialize();
@@ -40,6 +42,12 @@ public class PCuserLogin extends JFrame implements ActionListener {
 		frame.getContentPane().setLayout(null);
 		frame.setVisible(true);
 
+		pcseat = new JLabel("pc방 자리");
+		pcseat.setForeground(Color.WHITE);
+		pcseat.setFont(new Font("굴림", Font.PLAIN, 20));
+		pcseat.setBounds(244, 101, 89, 33);
+		frame.getContentPane().add(pcseat);
+
 		IDlb = new JLabel("ID");
 		IDlb.setForeground(Color.WHITE);
 		IDlb.setFont(new Font("굴림", Font.PLAIN, 20));
@@ -51,6 +59,13 @@ public class PCuserLogin extends JFrame implements ActionListener {
 		PWDlb.setFont(new Font("굴림", Font.PLAIN, 20));
 		PWDlb.setBounds(212, 303, 184, 33);
 		frame.getContentPane().add(PWDlb);
+
+		Seat = AllNum.PCNum(Seat);
+
+		Jcom = new JComboBox(Seat);
+		Jcom.setBounds(442, 101, 246, 41);
+		frame.getContentPane().add(Jcom);
+		Jcom.setVisible(true);
 
 		IDText = new JTextField();
 		IDText.setBounds(442, 200, 246, 41);
@@ -80,14 +95,15 @@ public class PCuserLogin extends JFrame implements ActionListener {
 
 		BtnLogin.addActionListener(this);
 		BtnClear.addActionListener(this);
+		Jcom.addActionListener(this);
 
 	}
-	
+
 	public void Clear() {
 		IDText.setText("");
 		PWDText.setText("");
 	}
-	
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
@@ -96,33 +112,39 @@ public class PCuserLogin extends JFrame implements ActionListener {
 		if (e.getSource().equals(BtnLogin)) {
 			if (id.equals("") || id.equals(null)) {
 				JOptionPane.showMessageDialog(null, "아이디와 비밀번호를 확인 해주세요", "알림 창", JOptionPane.WARNING_MESSAGE);
+				return;
 			} else if (pwd.equals("") || pwd.equals(null)) {
 				JOptionPane.showMessageDialog(null, "아이디와 비밀번호를 확인 해주세요", "알림 창", JOptionPane.WARNING_MESSAGE);
-			} else if ((!id.equals("")) && (!id.equals(null)) && (!pwd.equals("")) && (!pwd.equals(null))) {
+				return;
+			} else if(Pccombo.equals("")||Pccombo.equals(null)||Pccombo.equals("좌석을 골라주세요")) {
+				JOptionPane.showMessageDialog(null, "좌석을 선택해주세요", "알림 창", JOptionPane.WARNING_MESSAGE);
+				return;
+			}else if ((!id.equals("")) && (!id.equals(null)) && (!pwd.equals("")) && (!pwd.equals(null))) {
 
 				// 회원
 				boolean Check = UserLoginDb.Setting(id, pwd);
 				if (Check) {
 					JOptionPane.showMessageDialog(null, "확인되었습니다.", "알림 창", JOptionPane.WARNING_MESSAGE);
 					frame.dispose();
-					
+
 				} else if (!Check) {
 					JOptionPane.showMessageDialog(null, "아이디와 비밀번호를 확인 해주세요", "알림 창", JOptionPane.WARNING_MESSAGE);
 				}
 			}
-			} else if (e.getSource().equals(BtnClear)) {
-				int result = JOptionPane.showConfirmDialog(null, "홈 화면으로 돌아 가시겠습니까?", "확인 메시지",
-						JOptionPane.YES_NO_OPTION);
+		} else if (e.getSource().equals(BtnClear)) {
+			int result = JOptionPane.showConfirmDialog(null, "홈 화면으로 돌아 가시겠습니까?", "확인 메시지", JOptionPane.YES_NO_OPTION);
 
-				if (result == JOptionPane.NO_OPTION || result == JOptionPane.CLOSED_OPTION) {
-						Clear();
-				} else if (result == JOptionPane.YES_OPTION) {
-						new MainPc();
-						frame.dispose();
-				}
-				
+			if (result == JOptionPane.NO_OPTION || result == JOptionPane.CLOSED_OPTION) {
+				Clear();
+			} else if (result == JOptionPane.YES_OPTION) {
+				new MainPc();
+				frame.dispose();
+			}
+
+		} else if (e.getSource().equals(Jcom)) {
+			Pccombo = Jcom.getSelectedItem().toString();
+
 		}
 	}
-
 
 }
